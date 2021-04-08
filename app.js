b4305699a775226bf869ac749de2e8ac088ff5c7
Rei Ito <Rei Ito99@gmail.com>
2021-04-09 05:55:42 +1000

    cleaner content open + dynamic experience section + switching content

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const header_name = document.querySelector('.header .nav-bar a h1');
+
 const contact_expand = document.querySelector('.footer .expand-footer img');
 const contact_h1 = document.querySelector('.footer h1');
 const contact = document.querySelector('.footer');
 const contact_form = document.querySelector('.fcf-body');
-
 const social_texts = document.querySelectorAll('.footer .social-text');
-const project_items = document.querySelectorAll('.projects .projects-row .project-item');
-const project_arrows = document.querySelectorAll('.projects .decor-arrow');
-const project_contents = document.querySelectorAll('.projects .project-content');
+
+const project_items = document.querySelectorAll('.project-item');
+const project_arrows = document.querySelectorAll('.decor-arrow');
+const project_contents = document.querySelectorAll('.project-content');
+const project_videos = document.querySelectorAll('.project-content iframe');
+const project_heading = document.querySelectorAll('.project-desc h1');
+const project_paragraph = document.querySelectorAll('.project-desc p');
 
 var footerOpen = false;
 
 var form = document.getElementById("fcf-form-id");
+var projectsData;
+var experienceData;
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadJSON(function(response) {
+        projectsData = JSON.parse(response);
+
+        projectsData.forEach((item, index) => {
+            document.querySelectorAll('.project-item')[index].classList.add('project-'+[index]);
+            document.querySelectorAll('.project-item h2')[index].textContent = item.title;
+            document.querySelector('.project-'+ [index]).style.backgroundImage = item.static_path;
+        })
+    }, 'project_data.json');
+
+    loadJSON(function(response) {
+        experienceData = JSON.parse(response);
+
+        experienceData.forEach((item,index) => {
+            document.querySelector('.experience-bottom').innerHTML += (
+                '<div class="experience-item">\n<h2>'+ item.title
+                + '</h2>\n<h3>' + item.employer
+                +'</h3>\n<h4>' + item.dates
+                + '</h4>\n<p>' + item.description
+                + '</p>\n</div>\n'
+            )
+
+            if (index != experienceData.length - 1)
+                document.querySelector('.experience-bottom').innerHTML += '<div class="arrow"><img src="https://img.icons8.com/windows/96/000000/up.png"/></div>\n';
+        })
+    }, 'experience_data.json');
+}, false);
 
 function findPos(obj) {
     var curtop = 0;
@@ -39,7 +74,20 @@ async function handleSubmit(event) {
     }).catch(error => {
       status.innerHTML = "Oops, here was a problem. Please use the below methods instead."
     });
-  }
+}
+
+function loadJSON(callback, fileName) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', fileName, true); // Replace 'appDataServices' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+}
 
 function toggleFooter()
 {
@@ -75,13 +123,13 @@ document.addEventListener('scroll', ()=> {
 	if (scroll_position > findPos(document.getElementById("show-name-pos")))
     {
 		header_name.style.fontSize = '4rem';
-        header_name.style.transform = 'scaleY(1)';
+        header_name.style.transform = 'scale(1)';
         header_name.style.opacity = '1';
 	}
     else 
     {
         header_name.style.fontSize = '0';
-        header_name.style.transform = 'scaleY(0)';
+        header_name.style.transform = 'scale(0)';
 		header_name.style.opacity = '0';
 	}
 
@@ -90,78 +138,76 @@ document.addEventListener('scroll', ()=> {
 })
 
 var activeItem = null;
-project_items.forEach(item => {
+project_items.forEach((item, index) => {
     item.addEventListener('click', () => {
-        if (Array.from(project_items).indexOf(item) < 3)
+        var roundedIndex = Math.floor(index / 3);
+        var activeRoundedIndex = Math.floor(Array.from(project_items).indexOf(activeItem) / 3)
+
+        if (!project_arrows[roundedIndex].classList.contains('active'))
         {
-            if (project_contents[0].style.display === "none" || project_contents[0].style.display === "")
+            toggleProjectContent(roundedIndex, true);
+            window.scroll(0, (findPos(project_arrows[roundedIndex]) - 368));
+
+            if (activeRoundedIndex>=0)
             {
-                console.log("opening content");
+                if (project_arrows[activeRoundedIndex].classList.contains('active'))
+                {
+                    toggleProjectContent(activeRoundedIndex, false);
 
-                project_arrows[0].style.display = "flex";
-                project_contents[0].style.display = "flex";
-                
-                project_arrows[1].style.display = "none";
-                project_contents[1].style.display = "none";
-
-                window.scroll(0, (findPos(document.getElementById("first-row-pos")) - 80));
+                    if (roundedIndex != 0)
+                        window.scroll(0, (findPos(project_arrows[roundedIndex]) - 958));
+                }
             }
-            else if (item === activeItem)
-            {
-                project_arrows[0].style.display = "none";
-                project_contents[0].style.display = "none";
 
-                console.log("closing content");
-
-                window.scroll(0, findPos(document.getElementById("projects")));
-            }
+            switchProjectItemDetails(roundedIndex, index, item);
+        }
+        else if (item === activeItem)
+        {
+            toggleProjectContent(activeRoundedIndex, false);
+            window.scroll(0, findPos(document.getElementById("projects")));
+            activeItem = null;
         }
         else
-        {
-            if (project_arrows[1].style.display === "none") 
-            {
-                project_arrows[1].style.display = "flex";
-                project_contents[1].style.display = "flex";
+            switchProjectItemDetails(roundedIndex, index, item);
+    })
 
-                project_arrows[0].style.display = "none";
-                project_contents[0].style.display = "none";
+    item.addEventListener('mouseenter', () => {
+        document.querySelector('.project-'+[index]).style.backgroundImage = projectsData[index].gif_path;
+    })
 
-                window.scroll(0, (findPos(document.getElementById("second-row-pos")) - 110));
-            }
-            else if (item === activeItem)
-            {
-                project_arrows[1].style.display = "none";
-                project_contents[1].style.display = "none";
-
-                window.scroll(0, findPos(document.getElementById("projects")));
-            }
-        }
-
-        switch (Array.from(project_items).indexOf(item))
-        {
-            case 0:
-                project_arrows[0].style.marginRight = "900px";
-                break;
-            case 1:
-                project_arrows[0].style.marginRight = "0px";
-                break;
-            case 2:
-                project_arrows[0].style.marginRight = "-900px";
-                break;
-            case 3:
-                project_arrows[1].style.marginRight = "900px";
-                break;
-            case 4:
-                project_arrows[1].style.marginRight = "0px";
-                break;
-            case 5:
-                project_arrows[1].style.marginRight = "-900px";
-                break;
-        }
-
-        activeItem = item;
+    item.addEventListener('mouseleave', () => {
+        document.querySelector('.project-'+[index]).style.backgroundImage = projectsData[index].static_path;
     })
 })
+
+function switchProjectItemDetails(roundedIndex, index, item)
+{
+    activeItem = item;
+        
+    project_videos[roundedIndex].src = projectsData[index].video_link;
+    project_heading[roundedIndex].textContent = projectsData[index].title;
+    project_paragraph[roundedIndex].textContent = projectsData[index].description;
+
+    switch (index % 3)
+    {
+        case 0:
+            project_arrows[roundedIndex].style.marginRight = "900px";
+            break;
+        case 1:
+            project_arrows[roundedIndex].style.marginRight = "0px";
+            break;
+        case 2:
+            project_arrows[roundedIndex].style.marginRight = "-900px";
+            break;
+    }
+}
+
+function toggleProjectContent(index, isActive)
+{
+    project_arrows[index].classList.toggle('active', isActive);
+    project_contents[index].classList.toggle('active', isActive);
+    project_videos[index].classList.toggle('active', isActive);
+}
 
 contact_expand.addEventListener('click', toggleFooter);
 
@@ -170,4 +216,4 @@ contact.addEventListener('transitionend', () => {
         window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
 });
 
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
