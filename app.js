81dd3c0e68160c96b3b77c60351cd0d9569090f0
Rei Ito <Rei Ito99@gmail.com>
2021-04-10 03:59:10 +1000

    dynamic projects section + rough animations

--- app.js
+++ app.js
@@ -6,40 +6,136 @@ const contact = document.querySelector('.footer');
 const contact_form = document.querySelector('.fcf-body');
 const social_texts = document.querySelectorAll('.footer .social-text');
 
-const project_items = document.querySelectorAll('.project-item');
-const project_arrows = document.querySelectorAll('.decor-arrow');
-const project_contents = document.querySelectorAll('.project-content');
-const project_videos = document.querySelectorAll('.project-content iframe');
-const project_heading = document.querySelectorAll('.project-desc h1');
-const project_paragraph = document.querySelectorAll('.project-desc p');
+const test = document.querySelector('.experience-item h2');
 
 var footerOpen = false;
 
 var form = document.getElementById("fcf-form-id");
 var projectsData;
 var experienceData;
+var activeItem = null;
 
 document.addEventListener('DOMContentLoaded', function() {
-    loadJSON(function(response) {
-        projectsData = JSON.parse(response);
 
+    loadJSON(function(response) {
+
+        projectsData = JSON.parse(response);
         projectsData.forEach((item, index) => {
-            document.querySelectorAll('.project-item')[index].classList.add('project-'+[index]);
-            document.querySelectorAll('.project-item h2')[index].textContent = item.title;
-            document.querySelector('.project-'+ [index]).style.backgroundImage = item.static_path;
+            if ((index % 3) === 0)
+            {
+                document.querySelector('.projects').innerHTML += (
+                    '<div class="projects-row">\n' +
+                        '<div class="project-item project-card-color">\n' +
+                            '<h2>' + projectsData[index].title + '</h2>\n' +
+                        '</div>\n' +
+                        '<div class="project-item project-card-color">\n' +
+                            '<h2>' + projectsData[index+1].title + '</h2>\n' +
+                        '</div>\n' +
+                        '<div class="project-item project-card-color">\n' +
+                            '<h2>' + projectsData[index+2].title + '</h2>\n' +
+                        '</div>\n' +
+                    '</div>\n' +
+                    '<div class="decor-arrow">\n' +
+                        '<img src="https://img.icons8.com/fluent-systems-filled/96/000000/sort-up.png"/>\n' +
+                    '</div>\n' +
+                    '<div class="project-content">\n' +
+                        '<div class="project-video">\n' +
+                            '<iframe width="640" height="360" src="' + projectsData[index].video_link + '"></iframe>\n' +
+                        '</div>\n' +
+                        '<div class="project-desc">\n' +
+                            '<h1>' + projectsData[index].title + '</h1>\n' +
+                            '<p>' + projectsData[index].description + '</p>\n' +
+                            '<div class="project-icons">\n' +
+                                '<div class="project-link">\n' +
+                                    '<a href="' + projectsData[index].github_link + '">\n' +
+                                        '<img src="https://img.icons8.com/dusk/128/000000/github.png"/>\n' +
+                                    '</a>\n' +
+                                '</div>\n' +
+                                '<div class="project-link">\n' +
+                                    '<a href="' + projectsData[index].itchio_link + '">\n' +
+                                        '<img src="https://img.icons8.com/dusk/128/000000/itch-io.png"/>\n' +
+                                    '</a>\n' +
+                                '</div>\n' +
+                            '</div>\n' +
+                        '</div>\n' +
+                    '</div>\n'
+                )
+            }
+        })
+
+        document.querySelectorAll('.project-item').forEach((item, index) => {
+            var roundedIndex = Math.floor(index / 3);
+
+            item.classList.add('project-'+[index]);
+            document.querySelector('.project-'+ [index]).style.backgroundImage = projectsData[index].static_path;
+
+            item.addEventListener('click', () => {
+                document.querySelectorAll('.project-video')[roundedIndex].style.animation = 'none';
+                document.querySelectorAll('.project-desc')[roundedIndex].style.animation = 'none';
+                document.querySelectorAll('.project-video')[roundedIndex].offsetHeight; /* trigger reflow */
+                document.querySelectorAll('.project-desc')[roundedIndex].offsetHeight; /* trigger reflow */
+                document.querySelectorAll('.project-video')[roundedIndex].style.animation = '';
+                document.querySelectorAll('.project-desc')[roundedIndex].style.animation = '';
+
+                var activeRoundedIndex = Math.floor(Array.from(document.querySelectorAll('.project-item')).indexOf(activeItem) / 3)
+        
+                if (!document.querySelectorAll('.decor-arrow')[roundedIndex].classList.contains('active'))
+                {
+                    if (document.querySelectorAll('.project-content iframe')[roundedIndex].src != projectsData[index].video_link)
+                    {
+                        document.querySelectorAll('.project-content iframe')[roundedIndex].src = projectsData[index].video_link;                 
+                        sleep(700);
+                    }
+
+                    switchProjectItemDetails(roundedIndex, index, item);
+                    
+                    toggleProjectContent(roundedIndex, true);
+                    window.scroll(0, (findPos(document.querySelectorAll('.decor-arrow')[roundedIndex]) - 368));
+        
+                    if (activeRoundedIndex>=0)
+                    {
+                        if (document.querySelectorAll('.decor-arrow')[activeRoundedIndex].classList.contains('active'))
+                        {
+                            toggleProjectContent(activeRoundedIndex, false);
+        
+                            if (roundedIndex != 0)
+                                window.scroll(0, (findPos(document.querySelectorAll('.decor-arrow')[roundedIndex]) - 958));
+                        }
+                    }
+                }
+                else if (item === activeItem)
+                {
+                    toggleProjectContent(activeRoundedIndex, false);
+                    window.scroll(0, findPos(document.getElementById("projects")));
+                    activeItem = null;
+                }
+                else
+                {
+                    document.querySelectorAll('.project-content iframe')[roundedIndex].src = projectsData[index].video_link;  
+                    switchProjectItemDetails(roundedIndex, index, item);
+                }
+            })
+        
+            item.addEventListener('mouseenter', () => {
+                document.querySelector('.project-'+[index]).style.backgroundImage = projectsData[index].gif_path;
+            })
+        
+            item.addEventListener('mouseleave', () => {
+                document.querySelector('.project-'+[index]).style.backgroundImage = projectsData[index].static_path;
+            })
         })
     }, 'project_data.json');
 
     loadJSON(function(response) {
         experienceData = JSON.parse(response);
-
         experienceData.forEach((item,index) => {
             document.querySelector('.experience-bottom').innerHTML += (
-                '<div class="experience-item">\n<h2>'+ item.title
-                + '</h2>\n<h3>' + item.employer
-                +'</h3>\n<h4>' + item.dates
-                + '</h4>\n<p>' + item.description
-                + '</p>\n</div>\n'
+                '<div class="experience-item">\n' +
+                    '<h2>'+ item.title + '</h2>\n' +
+                    '<h3>' + item.employer +'</h3>\n' +
+                    '<h4>' + item.dates + '</h4>\n' +
+                    '<p>' + item.description + '</p>\n' +
+                '</div>\n'
             )
 
             if (index != experienceData.length - 1)
@@ -47,6 +143,14 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }, 'experience_data.json');
 }, false);
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
 
 function findPos(obj) {
     var curtop = 0;
@@ -100,6 +204,8 @@ function toggleFooter()
     });
 
     footerOpen = !footerOpen;
+
+    console.log(document.querySelectorAll('.project-item').length);
 }
 
 function retractFooter()
@@ -137,76 +243,32 @@ document.addEventListener('scroll', ()=> {
         retractFooter();
 })
 
-var activeItem = null;
-project_items.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        var roundedIndex = Math.floor(index / 3);
-        var activeRoundedIndex = Math.floor(Array.from(project_items).indexOf(activeItem) / 3)
-
-        if (!project_arrows[roundedIndex].classList.contains('active'))
-        {
-            toggleProjectContent(roundedIndex, true);
-            window.scroll(0, (findPos(project_arrows[roundedIndex]) - 368));
-
-            if (activeRoundedIndex>=0)
-            {
-                if (project_arrows[activeRoundedIndex].classList.contains('active'))
-                {
-                    toggleProjectContent(activeRoundedIndex, false);
-
-                    if (roundedIndex != 0)
-                        window.scroll(0, (findPos(project_arrows[roundedIndex]) - 958));
-                }
-            }
-
-            switchProjectItemDetails(roundedIndex, index, item);
-        }
-        else if (item === activeItem)
-        {
-            toggleProjectContent(activeRoundedIndex, false);
-            window.scroll(0, findPos(document.getElementById("projects")));
-            activeItem = null;
-        }
-        else
-            switchProjectItemDetails(roundedIndex, index, item);
-    })
-
-    item.addEventListener('mouseenter', () => {
-        document.querySelector('.project-'+[index]).style.backgroundImage = projectsData[index].gif_path;
-    })
-
-    item.addEventListener('mouseleave', () => {
-        document.querySelector('.project-'+[index]).style.backgroundImage = projectsData[index].static_path;
-    })
-})
-
 function switchProjectItemDetails(roundedIndex, index, item)
 {
     activeItem = item;
-        
-    project_videos[roundedIndex].src = projectsData[index].video_link;
-    project_heading[roundedIndex].textContent = projectsData[index].title;
-    project_paragraph[roundedIndex].textContent = projectsData[index].description;
+    
+    document.querySelectorAll('.project-desc h1')[roundedIndex].textContent = projectsData[index].title;
+    document.querySelectorAll('.project-desc p')[roundedIndex].textContent = projectsData[index].description;
 
     switch (index % 3)
     {
         case 0:
-            project_arrows[roundedIndex].style.marginRight = "900px";
+            document.querySelectorAll('.decor-arrow')[roundedIndex].style.marginRight = "900px";
             break;
         case 1:
-            project_arrows[roundedIndex].style.marginRight = "0px";
+            document.querySelectorAll('.decor-arrow')[roundedIndex].style.marginRight = "0px";
             break;
         case 2:
-            project_arrows[roundedIndex].style.marginRight = "-900px";
+            document.querySelectorAll('.decor-arrow')[roundedIndex].style.marginRight = "-900px";
             break;
     }
 }
 
 function toggleProjectContent(index, isActive)
 {
-    project_arrows[index].classList.toggle('active', isActive);
-    project_contents[index].classList.toggle('active', isActive);
-    project_videos[index].classList.toggle('active', isActive);
+    document.querySelectorAll('.decor-arrow')[index].classList.toggle('active', isActive);
+    document.querySelectorAll('.project-content')[index].classList.toggle('active', isActive);
+    document.querySelectorAll('.project-video iframe')[index].classList.toggle('active', isActive);
 }
 
 contact_expand.addEventListener('click', toggleFooter);
