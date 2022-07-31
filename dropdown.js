d696075904ee0b00c9350a9b1da9bb09a672680c
Rei Ito <Rei Ito99@gmail.com>
2022-08-01 01:25:50 +1000

    new information + image fix

--- dropdown.js
+++
@@ -1,6 +0,0 @@
-const expand = document.querySelector('.dropdown-button');
-
-document.querySelector('.dropdown-button').addEventListener('click', () => {
-    document.querySelector('.dropdown img').classList.toggle('active');
-    document.querySelector('.dropdown-content').classList.toggle('active');
-});
